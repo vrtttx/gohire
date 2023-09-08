224672aeed80ba484220800191dcbf649037912c
ynncstslv <ynncstslv@gmail.com>
2023-09-08 13:15:41 -0300

    fix: handler init

--- handler/handler.go
+++ handler/handler.go
@@ -1,1 +1,16 @@
 package handler
+
+import (
+	"github.com/vrtttx/gohire/config"
+	"gorm.io/gorm"
+)
+
+var (
+	db *gorm.DB
+	logger *config.Logger
+)
+
+func InitializeHandler() {
+	logger = config.GetLogger("handler")
+	db = config.GetSQLite()
+}
