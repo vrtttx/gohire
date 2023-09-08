0fec175632c0ac362465b1ffb9c79c9ccc408de1
ynncstslv <ynncstslv@gmail.com>
2023-09-08 12:30:30 -0300

    feat: logger implementation

--- main.go
+++ main.go
@@ -1,10 +1,25 @@
 package main
 
 import (
+	"github.com/vrtttx/gohire/config"
 	"github.com/vrtttx/gohire/router"
 )
 
+var (
+	logger *config.Logger
+)
+
 func main() {
+	logger = config.GetLogger("main")
+
+	// initialize configs
+	err := config.Init()
+
+	if err != nil {
+		logger.Errorf("CONFIG_INIT_ERROR: %v", err)
+		return
+	}
+
 	// initialize router
 	router.Initialize()
 }
